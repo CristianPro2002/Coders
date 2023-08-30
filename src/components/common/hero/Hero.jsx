67e4297a565cea45c493207a6dd1842527bc99fb
CristianPro2002 <cristian.0713.cs@gmail.com>
2023-08-30 11:17:19 -0500

    prueba

--- src/components/common/hero/Hero.jsx
+++ src/components/common/hero/Hero.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { THEME_1, THEME_2 } from "../../../utils/constants/theme";
 import ThemeOne from "./themes-hero/first-theme/ThemeOne";
 import ThemeTwo from "./themes-hero/second-theme/ThemeTwo";
@@ -7,14 +7,13 @@ import ThemeTwo from "./themes-hero/second-theme/ThemeTwo";
 export default function Hero() {
   const dataPlace = useSelector((state) => state.place);
   const { theme } = useSelector((state) => state.theme);
-  const location = window.location.pathname;
-
+  const location = useLocation();
   return (
     <>
       {theme === THEME_1 ? (
-        <ThemeOne dataPlace={dataPlace} Link={Link} location={location} />
+        <ThemeOne dataPlace={dataPlace} Link={Link} location={location.pathname} />
       ) : theme === THEME_2 ? (
-        <ThemeTwo dataPlace={dataPlace} Link={Link} location={location} />
+        <ThemeTwo dataPlace={dataPlace} Link={Link} location={location.pathname} />
       ) : null}
     </>
   );
